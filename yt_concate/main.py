af2f54eb22d86a78f20fc95457820fafcfb534e1
Noel <as11111satw@gapp.nthu.edu.tw>
2021-12-08 23:28:25 +0800

    Refactor code into pipeline design pattern

--- yt_concate/main.py
+++ yt_concate/main.py
@@ -1,38 +1,21 @@
-import urllib.request
-import json
+from yt_concate.pipeline.pipeline import Pipeline
+from yt_concate.pipeline.steps.get_video_list import GetVideoList
 
-
-from yt_concate.settings import API_KEY
-
-from dotenv import load_dotenv
 CHANNEL_ID = "UCTnK3UFznEB5bd4vDEFMM4A"
 
 
-def get_all_video_in_channel(channel_id):
-    api_key = API_KEY
-    base_video_url = 'https://www.youtube.com/watch?v='
-    base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
+def main():
+    steps = [
+        GetVideoList(),
+    ]
 
-    first_url = base_search_url + 'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(api_key,
-                                                                                                        channel_id)
+    inputs = {
+        "channel_id": CHANNEL_ID
+    }
 
-    video_links = []
-    url = first_url
-    while True:
-        inp = urllib.request.urlopen(url)
-        resp = json.load(inp)
-
-        for i in resp['items']:
-            if i['id']['kind'] == "youtube#video":
-                video_links.append(base_video_url + i['id']['videoId'])
-
-        try:
-            next_page_token = resp['nextPageToken']
-            url = first_url + '&pageToken={}'.format(next_page_token)
-        except KeyError:
-            break
-    return video_links
+    p = Pipeline(steps)
+    p.run(inputs)
 
 
-video_list = get_all_video_in_channel(CHANNEL_ID)
-print(video_list)
+if __name__ == "__main__":
+    main()
